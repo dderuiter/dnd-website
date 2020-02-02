6d986a6e838cf91c5948fbaf980cda83e32b15ad
dnd <davisderuiter@gmail.com>
2020-02-01 23:49:30 -0500

    Initial coding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProfileComponent } from './sections/profile/profile.component';
-import { JumbotronComponent } from './jumbotron/jumbotron.component';
-import { ExperienceComponent } from './sections/experience/experience.component';
-import { ProjectsComponent } from './sections/projects/projects.component';
-import { SkillsComponent } from './sections/skills/skills.component';
-import { EducationComponent } from './sections/education/education.component';
-import { SocialLinksComponent } from './social-links/social-links.component';
-import { HomeLinkComponent } from './home-link/home-link.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { ProfileComponent } from './modules/sections/profile/profile.component';
+import { JumbotronComponent } from './modules/jumbotron/jumbotron.component';
+import { ExperienceComponent } from './modules/sections/experience/experience.component';
+import { ProjectsComponent } from './modules/sections/projects/projects.component';
+import { SkillsComponent } from './modules/sections/skills/skills.component';
+import { EducationComponent } from './modules/sections/education/education.component';
+import { SocialLinksComponent } from './modules/social-links/social-links.component';
+import { HomeLinkComponent } from './modules/home-link/home-link.component';
 
 @NgModule({
     declarations: [
@@ -32,6 +33,7 @@ import { HomeLinkComponent } from './home-link/home-link.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
         MDBBootstrapModule.forRoot()
     ],
     providers: [],
---
+++ src/app/modules/sections/experience/experience.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ExperienceTagService } from '../../../services/experience-tag.service';
+
+@Component({
+    selector: 'app-experience',
+    templateUrl: './experience.component.html',
+    styleUrls: ['./experience.component.scss']
+})
+export class ExperienceComponent implements OnInit {
+
+    activeTabNum: number;
+    tags: Array<string>;
+
+    constructor(private experienceTagService: ExperienceTagService) { }
+
+    ngOnInit() {
+        this.showTab(0);
+    }
+
+    showTab(tabNum) {
+        this.activeTabNum = tabNum;
+
+        this.experienceTagService.getTags().subscribe(data => {
+            switch (tabNum) {
+                case 0:
+                    this.tags = data["2017"];
+                    break;
+                case 1:
+                    this.tags  = data["2016"];
+                    break;
+                case 2:
+                    this.tags  = data["2015"];
+                    break;
+                case 3:
+                    this.tags  = data["2014"];
+                    break;
+                default:
+                    this.tags  = [];
+            }
+            for(let i = 0; i < this.tags.length; i++) {
+                console.log(this.tags[i]);
+            }
+            console.log("-------------------------------");
+        });
+    }
+
+}
