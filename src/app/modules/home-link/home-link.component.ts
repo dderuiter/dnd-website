72ae295e5d318c6ac5e6e299d27cec159676a368
dnd <davisderuiter@gmail.com>
2020-02-04 21:04:38 -0500

    Initial coding

--- src/app/modules/home-link/home-link.component.ts
+++ src/app/modules/home-link/home-link.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home-link',
-  templateUrl: './home-link.component.html',
-  styleUrls: ['./home-link.component.scss']
+    selector: 'app-home-link',
+    templateUrl: './home-link.component.html',
+    styleUrls: ['./home-link.component.scss']
 })
 export class HomeLinkComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+    }
 
 }
