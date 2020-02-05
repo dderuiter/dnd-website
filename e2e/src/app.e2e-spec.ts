41d1510dbb7f97c1ab342bd337c2eed4d103c791
dnd <davisderuiter@gmail.com>
2020-02-04 21:26:01 -0500

    Initial coding

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dnd-website app is running!');
+    expect(page.getTitleText()).toEqual('dnd-website-v2 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dnd-website'`, () => {
+  it(`should have as title 'dnd-website-v2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('dnd-website');
+    expect(app.title).toEqual('dnd-website-v2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dnd-website app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('dnd-website-v2 app is running!');
   });
 });
